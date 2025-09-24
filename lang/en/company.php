69c4e5978b1ee1dfccd3da66c4b9d9a8e7d7b466
Tharusha Rashmika Sandakelum <103655595+Tharusha200219@users.noreply.github.com>
2025-09-25 00:09:41 +0530

    Remove unused mail templates, routes, and test files; clean up storage directory structure and configuration.

--- lang/en/company.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'name' => 'Name of the company',
-    'logo' => 'Logo',
-    'emp_prefix' => 'Prefix of employee number',
-    'status' => 'Status'
-];
--- lang/en/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'company_name' => 'Name of the company',
-    'company_email' => 'Company Primary Email',
-    'company_phone' => 'Contact Number',
-    'company_address' => 'Address',
-    'mail_signature' => 'Signature',
-];
