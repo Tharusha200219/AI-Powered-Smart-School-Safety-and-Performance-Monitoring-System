be50b2383fedb23f121a8ecbd12d645df0e86491
DilmiSamaranayaka <128679465+DilmiSamaranayaka@users.noreply.github.com>
2025-11-21 20:23:58 +0530

    add some new changes

--- realtime_mic.py
+++ realtime_mic.py
@@ -28,6 +28,32 @@ def record_audio():
     write(temp_path, SAMPLE_RATE, audio)
     return temp_path
 
+def predict_from_mic():
+    temp_file = record_audio()
 
+    mel = audio_to_melspectrogram(temp_file)
+    if mel is None:
+        print("❌ Error converting microphone audio.")
+        return
 
-  
+    mel = np.expand_dims(mel, axis=0)
+
+    # Predict
+    preds = model.predict(mel)
+    idx = np.argmax(preds)
+    confidence = float(np.max(preds))
+    label = label_classes[idx]
+
+    print("\n🔊 REAL-TIME DETECTION")
+    print("--------------------------")
+    print("Detected:", label)
+    print("Confidence:", round(confidence, 3))
+    print("--------------------------")
+
+# Continuous loop
+print("🔥 Real-time audio detection started!")
+print("Press ENTER to analyze another 2 seconds or CTRL+C to stop.")
+
+while True:
+    input("\nPress ENTER to record...")
+    predict_from_mic()
