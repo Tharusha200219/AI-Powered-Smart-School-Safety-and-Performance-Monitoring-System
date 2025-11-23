fbf9c5d3002b89fac450cde02f8623f29cd61d63
Susiru24 <susirusampath24@gmail.com>
2025-11-24 03:26:35 +0530

    feat: add LeftBehindObjectDetector skeleton (YOLOv8)
    - Created src/models/ package
    - Implemented basic object detection with target class filtering
    - Ready for tracking integration next

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+"""Models module"""
+
+from .object_detector import LeftBehindObjectDetector
+from .threat_detector import ThreatDetector
+
+__all__ = ['LeftBehindObjectDetector', 'ThreatDetector']
+
